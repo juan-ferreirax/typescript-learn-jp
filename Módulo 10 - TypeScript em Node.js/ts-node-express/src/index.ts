0eabf0a9322abc98423b4cee1b05b614d1ec0773
Juan Pablo <juanpablofc8.0@gmail.com>
2026-05-24 12:40:05 -0300

    feat: Módulo 10 - TypeScript em Node.js.

---
+++ Módulo 10 - TypeScript em Node.js/ts-node-express/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello, Developers! Bem vindo(as) ao TypeScript com Express.js!</h1>');
+});
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
