fb408b61791466cd2c926d4c44093e095c0aca59
Juan Pablo <juanpablofc8.0@gmail.com>
2026-05-24 12:28:34 -0300

    feat: Módulo 1 - TypeScript.

---
+++ Módulo 1 - TypeScript/helloworld/helloworld.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Exemplo de Sintaxe: console.log('Hello World!');
+let mensagem = 'Hello World!';
+console.log(mensagem);
---
+++ Módulo 1 - TypeScript/helloworld/helloworld.ts
@@ -0,0 +1,3 @@
+// Exemplo de Sintaxe: console.log('Hello World!');
+let mensagem: string = 'Hello World!';
+console.log(mensagem)
